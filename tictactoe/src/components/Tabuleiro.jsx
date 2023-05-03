ed4eb36a0618884a6c667416c9e84b5cd88c5f5f
Arthur Viegas <arthur.viegas01@edu.pucrs.br>
2023-05-02 22:02:13 -0300

    Axios com get funcional (alterar para post e integrar ocm o backend)

--- tictactoe/src/components/Tabuleiro.jsx
+++ tictactoe/src/components/Tabuleiro.jsx
@@ -1,12 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react';
 import {Caixa} from "./Caixa";
-import "./Tabuleiro.css"
+import "./Tabuleiro.css";
+import axios from "axios";
 
 export const Tabuleiro = ({tabuleiro, onClick}) => {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const getAxios = () => {
+    setIsLoading(true);
+    axios.get("")
+      .then(response => {
+        setData(response.data);
+        console.log(data);
+      })
+      .catch(error => {
+        console.log(error);
+        console.log(isLoading)
+      });
+  };
+
+
   return (
     <div className='tabuleiro'>
         {tabuleiro.map((value, indice)=>{
-            return <Caixa value={value} onClick={() => onClick(indice)}/>
+            return <Caixa value={value} onClick={() => {
+              onClick(indice);
+              getAxios();
+            }}/>
         })}
     </div>
   )
